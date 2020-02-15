1b5dd949d7cdd3c669f1883e4c79e331be9206ee
natsuki-kining <natsuki_kining@126.com>
2020-02-15 21:18:16 +0800

    新增字体

--- video/artificial-intelligence/mz-2018/24-data-analysis/da-02-matplotlib.py
+++ video/artificial-intelligence/mz-2018/24-data-analysis/da-02-matplotlib.py
@@ -1,17 +1,22 @@
 # coding=utf-8
 from matplotlib import pyplot as plt
-import random
 from matplotlib import font_manager
+import random
 
-#windws和linux设置字体的放
-# font = {'family' : 'MicroSoft YaHei',
+
+"""
+matplotlib 默认不支持中文字符
+
+"""
+#windws和linux设置字体的方式
+# font = {'family' : 'SIMYOU.TTF',
 #         'weight': 'bold',
 #         'size': 'larger'}
-# matplotlib.rc("font",**font)
-# matplotlib.rc("font",family='MicroSoft YaHei',weight="bold")
+# font_manager.rc("font",**font)
+# font_manager.rc("font",family='SIMYOU.TTF',weight="bold")
 
 #另外一种设置字体的方式
-my_font = font_manager.FontProperties(fname="/System/Library/Fonts/PingFang.ttc")
+font = font_manager.FontProperties(fname="test.ttf")
 
 x = range(0,120)
 y = [random.randint(20,35) for i in range(120)]
@@ -24,11 +29,12 @@ plt.plot(x,y)
 _xtick_labels = ["10点{}分".format(i) for i in range(60)]
 _xtick_labels += ["11点{}分".format(i) for i in range(60)]
 #取步长，数字和字符串一一对应，数据的长度一样
-plt.xticks(list(x)[::3],_xtick_labels[::3],rotation=45,fontproperties=my_font) #rotaion旋转的度数
+plt.xticks(list(x)[::3],_xtick_labels[::3],rotation=45,fontproperties=font) #rotaion旋转的度数
+# plt.xticks(list(x)[::3],_xtick_labels[::3],rotation=90)
 
 #添加描述信息
-plt.xlabel("时间",fontproperties=my_font)
-plt.ylabel("温度 单位(℃)",fontproperties=my_font)
-plt.title("10点到12点每分钟的气温变化情况",fontproperties=my_font)
+# plt.xlabel("时间",fontproperties=font)
+# plt.ylabel("温度 单位(℃)",fontproperties=font)
+# plt.title("10点到12点每分钟的气温变化情况",fontproperties=font)
 
 plt.show()
